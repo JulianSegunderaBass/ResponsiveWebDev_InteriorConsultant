0ae1480ea7d6110d7e1b490ce02d857c0a8f6c97
LAPTOP-2KEC0QRP\Julian Bass <julianterrybass@gmail.com>
2022-01-15 19:37:51 +0800

    Mobile navigation

---
+++ script.js
@@ -0,0 +1,14 @@
+const openNavBtn = document.querySelector('.mobile-nav-btn');
+const navLinks = document.querySelectorAll('.nav-link');
+const closeNavBtn = document.querySelector('.close');
+const mobileNav = document.querySelector('.nav-links');
+
+openNavBtn.addEventListener('click', () => {
+  mobileNav.classList.add('open');
+});
+closeNavBtn.addEventListener('click', () => {
+  mobileNav.classList.remove('open');
+});
+navLinks.addEventListener('click', () => {
+  mobileNav.classList.remove('open');
+});
